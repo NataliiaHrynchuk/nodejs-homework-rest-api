75586c801839b01871ad40d782befededd1ab449
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-01-19 20:16:25 +0200

    add users model

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+// const Joi = require('joi');
+const { handleSchemaValidationErrors } = require('../helpers/index');
+
+const userSchema = Schema({
+    password: {
+        type: String,
+        minLength: [6, "password should be at least 6 characters long"],
+        required: [true, 'Set password for user'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [/[a-z0-9]+@[a-z0-9]+/, "User email is not valid"],
+    },
+    subscription: {
+        type: String,
+        enum: ["starter", "pro", "business"],
+        default: "starter"
+    },
+    token: String
+},
+    {versionKey: false,}
+);
+
+userSchema.post('save', handleSchemaValidationErrors);
+
+const User = model('user', userSchema);
+module.exports = {
+    User,
+    userSchema
+};
