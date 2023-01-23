0fd9a9a27bba2331d3227acd20bed3b5fd66ab7f
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2023-01-23 18:30:28 +0200

    add finde contacts by limit and favorite

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -2,63 +2,77 @@ const { Contact } = require('../models/contacts');
 const { RequestError } = require('../helpers/index');
 
 const getContacts = async (req, res, next) => {
-    const { _id } = req.user;
-    const { page = 1, limit = 20 } = req.query;
-    const skip = (page - 1) * limit;
-    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate('owner', '_id email');
+  const { _id } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  if (!favorite) {
+    const contacts = await Contact.find({ owner: _id }, '', {
+      skip,
+      limit: Number(limit),
+    }).populate('owner', '_id email');
     await res.json(contacts);
+  }
+  const contacts = await Contact.find({ owner: _id, favorite }, '', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', '_id email');
+  await res.json(contacts);
 };
 
 const getContact = async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await Contact.findById(contactId);
+  const { contactId } = req.params;
+  const contact = await Contact.findById(contactId);
 
-    if (!contact) {
-        return next(new RequestError(404, "Not found"));
-    }
+  if (!contact) {
+    return next(new RequestError(404, 'Not found'));
+  }
 
-    return res.json(contact);
+  return res.json(contact);
 };
 
 const addContact = async (req, res) => {
-    const {_id} = req.user;
-    const contact = await Contact.create({...req.body, owner: _id});
-    res.status(201).json(contact);
+  const { _id } = req.user;
+  const contact = await Contact.create({ ...req.body, owner: _id });
+  res.status(201).json(contact);
 };
 
 const updateContact = async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-    if (!contact) {
-        return next(new RequestError(404, "Not found"));
-    }
-    return res.status(200).json({ contact });
+  const { contactId } = req.params;
+  const contact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!contact) {
+    return next(new RequestError(404, 'Not found'));
+  }
+  return res.status(200).json({ contact });
 };
 
 const updateStatusContact = async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-    if (!contact) {
-        return next(new RequestError(404, "Not found"));
-    }
-    return res.status(200).json({ contact });
+  const { contactId } = req.params;
+  const contact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!contact) {
+    return next(new RequestError(404, 'Not found'));
+  }
+  return res.status(200).json({ contact });
 };
 
 const removeContact = async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await Contact.findByIdAndRemove(contactId);
-    if (!contact) {
-        throw new RequestError(404, "Not found");
-    }
-  
-    return res.status(200).json({ message: "Contact deleted" });
+  const { contactId } = req.params;
+  const contact = await Contact.findByIdAndRemove(contactId);
+  if (!contact) {
+    throw new RequestError(404, 'Not found');
+  }
+
+  return res.status(200).json({ message: 'Contact deleted' });
 };
 
 module.exports = {
-    getContacts,
-    getContact,
-    addContact,
-    updateContact,
-    updateStatusContact,
-    removeContact
-}
+  getContacts,
+  getContact,
+  addContact,
+  updateContact,
+  updateStatusContact,
+  removeContact,
+};
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,55 +1,65 @@
-const { Conflict, Unauthorized } = require("http-errors");
+const { Conflict, Unauthorized } = require('http-errors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-        throw new Conflict('Email in use');
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict('Email in use');
+  }
 
-    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    const result = await User.create({ email, password: hashPassword});
-    res.status(201).json(result);
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const result = await User.create({ email, password: hashPassword });
+  res.status(201).json(result);
 };
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw new Unauthorized('Email or password is wrong');
-    }
-    const passCompare = bcrypt.compareSync(password, user.password);
-    
-    if (!passCompare) {
-        throw new Unauthorized('Email or password is wrong');
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Unauthorized('Email or password is wrong');
+  }
+  const passCompare = bcrypt.compareSync(password, user.password);
 
-    const payload = {
-        id: user._id
-    };
+  if (!passCompare) {
+    throw new Unauthorized('Email or password is wrong');
+  }
 
-    const token = jwt.sign(payload, `${SECRET_KEY}`, { expiresIn: "1h" });
-    await User.findByIdAndUpdate(user._id, { token });
-    res.status(200).json(token);
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, `${SECRET_KEY}`, { expiresIn: '1h' });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.status(200).json(token);
 };
 
 const getCurrent = async (req, res) => {
-    const { email } = req.user;
-    res.status(200).json({ email });
+  const { email } = req.user;
+  res.status(200).json({ email });
 };
 const logout = async (req, res) => {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: null });
-    res.status(204).json();
- }
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: null });
+  res.status(204).json();
+};
+const updateSubscription = async (req, res, next) => {
+  const { _id } = req.user;
+  // const { subscription } = req.body;
+  const user = await User.findByIdAndUpdate(_id, req.body, { new: true });
+  if (!user) {
+    throw new Unauthorized('Email or password is wrong');
+  }
+  return res.status(200).json({ user });
+};
 
 module.exports = {
-    register,
-    login, 
-    getCurrent,
-    logout
-}
+  register,
+  login,
+  getCurrent,
+  logout,
+  updateSubscription,
+};
